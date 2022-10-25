20bbd76bf4b1286b7f3557d902315ca5481d21ab
Bassem Lessoued <lesd.bass@yahoo.fr>
2022-10-25 14:20:31 +0200

    hh

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api',Gouvernorat,Setup,Expediteur,Livreur,Colis,HistColis,RunSheet,Soldee,User)
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/buil'))
+    app.use(express.static('client/build'))
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
